184017753c7c39dbf86ea7fdd5bbade26d2ab182
Dakota <dakota.klasky@gmail.com>
2024-09-19 16:47:33 -0400

    write routes

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, make_response
 from flask_migrate import Migrate
+from sqlalchemy import func
 
 from models import db, Earthquake
 
@@ -21,6 +22,26 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake:
+        return make_response(earthquake.to_dict(),200)
+    else:
+        return make_response({"message": f'Earthquake {id} not found.'}, 404)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    earthquake_list = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    count = Earthquake.query.filter(Earthquake.magnitude >= magnitude).count()
+
+    result = []
+    for eq in earthquake_list:
+        result.append(eq.to_dict())
+
+    return make_response({"count":count,"quakes":result},200)
 
 
 if __name__ == '__main__':
